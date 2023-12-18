c3f83266e1e6c2fc41430fd1f24ebcdb3e06a17d
karthik narayan <developer@karthiknarayan.in>
2023-12-18 19:36:10 +0000

    nginx configured for all services

--- test.py
+++ test.py
@@ -1,26 +1,26 @@
-# sender.py
+# # sender.py
 
-import pika
+# import pika
 
-def send_message(message):
-    connection_params = pika.ConnectionParameters(
-        host='20.40.54.159',
-        port=8030,
-        credentials=pika.PlainCredentials(
-            username='karthik',
-            password='password'
-        )
-    )
+# def send_message(message):
+#     connection_params = pika.ConnectionParameters(
+#         host='20.40.54.159',
+#         port=8030,
+#         credentials=pika.PlainCredentials(
+#             username='karthik',
+#             password='password'
+#         )
+#     )
 
-    connection = pika.BlockingConnection(connection_params)
-    channel = connection.channel()
+#     connection = pika.BlockingConnection(connection_params)
+#     channel = connection.channel()
 
-    channel.queue_declare(queue='user_queue')
-    channel.basic_publish(exchange='', routing_key='user_queue', body=message)
+#     channel.queue_declare(queue='user_queue')
+#     channel.basic_publish(exchange='', routing_key='user_queue', body=message)
 
-    print(f" [x] Sent '{message}'")
+#     print(f" [x] Sent '{message}'")
 
-    connection.close()
+#     connection.close()
 
-# Example usage
-send_message('Hello RabbitMQ!')
+# # Example usage
+# send_message('Hello RabbitMQ!')
