fe5ecde23f1079146c723fc395cd3662c7ba1b3c
karthik narayan <developer@karthiknarayan.in>
2023-12-19 16:05:13 +0530

    mail conf changed

--- communicationservice/communicationservice/tasks.py
+++ communicationservice/communicationservice/tasks.py
@@ -12,7 +12,6 @@ def SendSignUpMail(payload):
                 "Welcome to the Karthik E-commerce here is your signup bonus of 1000 rupess coupon code: KARTHIK1000",
                 settings.EMAIL_HOST_USER,
                 [payload["email"],],
-                fail_silently=False,
         )
         
     except Exception as e:
